469efad2878ebdacc19ab0454de206e6d1683c7a
Nikolay Rademacher <hrrdx33@gmail.com>
2021-07-01 02:41:41 +0200

    feat: set up cypress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>RS Demo Fashion</title>
+      </Head>
+      <div className="subpixel-antialiased optimize-legibility text-gray-9">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
