19a743b4c85c546a69ac9973c211e1463bfc727d
Nikolay Rademacher <hrrdx33@gmail.com>
2021-07-05 18:39:19 +0200

    feat: add css reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import '../global-styles/globals.scss';
+import '../global-styles/index.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
