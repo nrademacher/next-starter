89ba238db728e3f2aadbef5765ea1dffe1cb3b3a
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2021-08-22 22:59:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     rules.forEach((rule) => {
       rule.use.forEach((moduleLoader) => {
         if (
-          moduleLoader.loader.includes('css-loader/dist') &&
+          moduleLoader.loader.includes('css-loader') &&
           typeof moduleLoader.options.modules === 'object'
         ) {
           moduleLoader.options = {
