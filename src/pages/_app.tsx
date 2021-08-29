2f80f98d2c8ecafb7ca277432ed8f3493c816189
Nikolay Rademacher <hrrdx33@gmail.com>
2021-08-30 00:39:29 +0200

    chore: add docker scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import '../global-styles/index.scss';
+import '@/styles/globals.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
