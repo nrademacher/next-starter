40fc2bd7d7bac099273ca62095c315ad2d554659
Nikolay Rademacher <hrrdx33@gmail.com>
2021-09-17 22:07:19 +0200

    feat: support absolute imports in jest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,13 @@
-import Head from 'next/head';
-import { Counter, Container, Main, CardGrid, Footer } from '@/components';
+import { Container, Main, Counter, CardGrid, Footer } from '@/components';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Container>
-        <Main>
-          <Counter />
-          <CardGrid />
-        </Main>
-        <Footer />
-      </Container>
-    </>
+    <Container>
+      <Main>
+        <Counter />
+        <CardGrid />
+      </Main>
+      <Footer />
+    </Container>
   );
 }
